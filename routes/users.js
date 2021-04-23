6b888c2c9813f985c14144ded4ffacc6135817be
Stacy <hristian.anastasia90@gmail.com>
2021-04-23 17:34:19 -0400

    Passport JWT

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const user = require("../models/user");
 const User = require("../models/user");
 const router = express.Router();
 const passport = require("passport");
+const authenticate = require("../authenticate");
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
@@ -30,9 +30,14 @@ router.post("/signup", (req, res) => {
 });
 
 router.post("/login", passport.authenticate("local"), (req, res) => {
+  const token = authenticate.getToken({ _id: req.user._id });
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
-  res.json({ success: true, status: "You are successfully logged in!" });
+  res.json({
+    success: true,
+    token: token,
+    status: "You are successfully logged in!",
+  });
 });
 
 router.get("/logout", (req, res, next) => {
